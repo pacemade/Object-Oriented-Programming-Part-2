0c8076fb990c088f4ca5ed64796600a37d4c099d
Danny Chang <dannychachang@gmail.com>
2017-12-18 18:40:34 -0500

    Add borrow and return to library, add overdue, looking to include books that actually are overdue

--- book.rb
+++ book.rb
@@ -2,16 +2,19 @@ class Book
 
   @@on_shelf = []
   @@on_loan = []
+  @@due_date = Time.now + 3600
 
   def initialize(title, author, isbn)
     @title = title
     @author = author
     @isbn = isbn
+    @lent_out = false
   end
 
   def self.new_book(title, author, isbn)
     new_book = Book.new(title, author, isbn)
     @@on_shelf << new_book
+    return new_book
   end
 
   def self.available
@@ -26,6 +29,64 @@ class Book
     return @@on_shelf.sample
   end
 
+  def lent_out?
+      lent = false
+      @@on_loan.each do |status|
+        if status == self
+          lent = true
+        end
+      end
+    return lent
+  end
 
+  def self.current_due_date
+    due_date = Time.now
+    return due_date
+  end
+
+  def borrow
+    temp = 0
+    if lent_out?
+      "This has already been taken out!"
+    else
+      @due_date = @@due_date
+      @@on_shelf.each do |book|
+        if book == self
+        temp = @@on_shelf.delete(book)
+        end
+      end
+        @@on_loan << temp
+    end
+  end
+
+  def return_to_library
+    back = 0
+    if lent_out? == false
+     "How can you return that which is not borrowed"
+    else
+      @@on_loan.each do |book|
+        if book == self
+          back = @@on_loan.delete(book)
+        end
+      end
+      @@on_shelf << back
+      @due_date = nil
+      return true
+    end
+  end
+
+  def due_date
+    return @due_date
+  end
+
+  def self.overdue
+    @@on_loan.each do |book|
+      if book.due_date > Time.now
+        return true
+      else
+        return "No Books overdue"
+      end
+    end
+  end
 
 end
--- main_book.rb
+++ main_book.rb
@@ -1,10 +1,25 @@
 require "./book.rb"
 
-new1 = Book.new_book("yes", "no", 123)
-new2 = Book.new_book("no", "yes", 321)
+new1 = Book.new_book("Nintendo Power", "Pierre", 2001)
+new2 = Book.new_book("Huehuehue", "Bob Loblaw", 321)
 
-p new1
-p new2
-p Book.available
 
 p Book.browse
+p Book.available
+p new1.borrow
+p Book.borrowed
+p Book.available
+
+p new1.lent_out?
+p new2.return_to_library
+p new1.return_to_library
+
+p Book.borrowed
+p Book.available
+
+p new1.borrow
+p new2.borrow
+
+p Book.borrowed
+
+p Book.overdue
